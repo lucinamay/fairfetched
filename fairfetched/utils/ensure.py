8f52420e31639a4c8cd796ab15edf41f4c79fef2
lucinamay <119406697+lucinamay@users.noreply.github.com>
2026-03-06 19:37:44 +0100

    IMP: cleaned up dependencies

--- fairfetched/utils/ensure.py
+++ fairfetched/utils/ensure.py
@@ -1,8 +1,8 @@
 import datetime
 import logging as lg
+import urllib.error
+import urllib.request
 from pathlib import Path
-
-import requests
 
 from ._track import track
 
@@ -15,16 +15,26 @@ def ensure_url(url: str, path: Path, force: bool = False) -> Path:
         lg.debug(f"File already exists at {path}. Skipping download.")
         return path
 
-    response = requests.get(url)
-    response.raise_for_status()
-    with open(path, "wb") as f:
-        total = int(response.headers.get("content-length", 0))
+    req = urllib.request.Request(url)
+    # urllib will raise HTTPError for non-2xx responses
+    with urllib.request.urlopen(req) as resp:
+        total_hdr = resp.getheader("Content-Length")
+        total = int(total_hdr) if total_hdr and total_hdr.isdigit() else 0
         chunk_size = 8192
-        for chunk in track(
-            response.iter_content(chunk_size=chunk_size),
-            total=(total // chunk_size) + int(total % chunk_size != 0),
-            desc=f"downloading {url.split('/')[-1]}",
-        ):
-            f.write(chunk)
+
+        def _iter_resp():
+            while True:
+                chunk = resp.read(chunk_size)
+                if not chunk:
+                    break
+                yield chunk
+
+        with open(path, "wb") as f:
+            for chunk in track(
+                _iter_resp(),
+                total=(total // chunk_size) + int(total % chunk_size != 0),
+                desc=f"downloading {url.split('/')[-1]}",
+            ):
+                f.write(chunk)
     lg.info(f"Downloaded {url} to {path} on {datetime.datetime.now()}")
     return path
