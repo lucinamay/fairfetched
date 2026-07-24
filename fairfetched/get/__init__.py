1395fd3ddb0113bc3f851df403c91a8ca5176794
lucinamay <119406697+lucinamay@users.noreply.github.com>
2026-07-24 18:32:16 +0200

    REF: renamed for clarity

--- fairfetched/get/__init__.py
+++ fairfetched/get/__init__.py
@@ -3,7 +3,7 @@ from .dataset import Chembl, Papyrus
 __all__ = ["Chembl", "Papyrus"]
 
 if __name__ == "__main__":
-    from .papyrus import ensure_raw, latest
+    from .papyrus import ensure_raw_files, latest
 
     print(latest())
-    ensure_raw(latest())
+    ensure_raw_files(latest())
