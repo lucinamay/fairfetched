1165a40cde1bf0feca9f3562d10566c5d180d571
lucinamay <119406697+lucinamay@users.noreply.github.com>
2026-07-26 19:16:39 +0200

    FIX: OOM-error caused by trying to scan a xz-compressed tsv of decompressed size of 40GB

--- fairfetched/utils/__init__.py
+++ fairfetched/utils/__init__.py
@@ -1,13 +1,17 @@
 from .ensure import ensure_url
 from .files import ensure_untarred_sqlite, file_suffix_from_url
-from .polars import ensure_sqlite_db_to_parquets, lowercase_columns, scan_tsvxz
+from .polars import (
+    decompress_and_scan_tsvxz,
+    ensure_sqlite_db_to_parquets,
+    lowercase_columns,
+)
 from .storage import BASE_DIR
 
 __all__ = [
     "BASE_DIR",
     ensure_url,
     lowercase_columns,
-    scan_tsvxz,
+    decompress_and_scan_tsvxz,
     file_suffix_from_url,
     ensure_sqlite_db_to_parquets,
     ensure_untarred_sqlite,
