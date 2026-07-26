4ec82c1225a86e04b26e2ee67d59b1d9f35f1ed9
lucina-may nollen <119406697+lucinamay@users.noreply.github.com>
2026-07-26 20:13:27 +0200

    FIX: homedir interpreted as literal
    * FIX: used expanduser() & ADD: tests
    
    * UPD: version

--- fairfetched/utils/storage.py
+++ fairfetched/utils/storage.py
@@ -5,11 +5,13 @@ from pathlib import Path
 
 def _get_fairfetched_home_dir() -> Path:
     if fairfetched_name := os.environ.get("FAIRFETCHED_HOME", None):
-        lg.debug(f"FAIRFETCHED_HOME as base directory: {Path(fairfetched_name)}")
-        return Path(fairfetched_name)
+        path = Path(fairfetched_name).expanduser()
+        lg.debug(f"FAIRFETCHED_HOME as base directory: {path}")
+        return path
     elif pystow_name := os.environ.get("PYSTOW_HOME", None):
-        lg.debug(f"using PYSTOW_HOME as base directory: {Path(pystow_name)}")
-        return Path(pystow_name)
+        path = Path(pystow_name).expanduser()
+        lg.debug(f"using PYSTOW_HOME as base directory: {path}")
+        return path
     return Path.home() / ".data"
 
 
